c2ab3dce445e66ce7cea8d468d3d742f135c377e
aaditeynim09 <aaditey.nim@gmail.com>
2026-03-11 15:34:47 +0530

    UC1: Add welcome message module

--- src/PalindromeApp.java
+++ src/PalindromeApp.java
@@ -1,5 +1,5 @@
 public class PalindromeApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker App");
+        System.out.println("Welcome to Palindrome Checker Application");
     }
 }
